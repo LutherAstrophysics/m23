c02cb7fa9b2cb1cc93962ee767a6c8275998dfb4
minhviet0308 <nguymi03@luther.edu>
2022-07-19 16:37:45 -0500

    format extraction output

--- usage/processing/test-extact.py
+++ usage/processing/test-extact.py
@@ -12,10 +12,10 @@ from m23.extract import extractStars
 refFile = r"C:\Data Processing\RefImage\ref_test.txt"
 imageFolder = r"C:\Data Processing\xxx\exp-test-idl"
 
-imageFiles = [os.path.join(imageFolder, rf"m23_7.0-0{number:02}.fit") for number in range(1, 2)]
+imageFiles = [os.path.join(imageFolder, rf"m23_7.0-0{number:02}.fit") for number in range(1, 20)]
 
 def makeLogFileName(index):
     return os.path.join(r"C:\Data Processing\xxx\Outputs", f"{index + 1}.txt")
 
 for index in range(len(imageFiles)):
-    extractStars(getdata(imageFiles[index]), refFile, makeLogFileName(index))
+    extractStars(getdata(imageFiles[index]), refFile, makeLogFileName(index), imageFiles[index])
