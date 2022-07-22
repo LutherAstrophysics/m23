c76f7564de2f570a1698ae546274921995139f04
Suman Chapai <sumanchapai@gmail.com>
2022-07-22 15:26:13 -0500

    add instructions for experiment folder

---
+++ experiment/README.md
@@ -0,0 +1,5 @@
+# Experiment folder
+
+Files in this folder won't be commited to git!
+
+This is a folder for roughwork, and playing with the module!
---
+++ experiment/test.py
@@ -0,0 +1,6 @@
+### Use this boilerplat in experiment if your experiment files
+### are directly inside the experiment folder (meaning not in 
+### subfolders)
+import sys
+if "../" not in sys.path:
+    sys.path.insert(0, "../")
