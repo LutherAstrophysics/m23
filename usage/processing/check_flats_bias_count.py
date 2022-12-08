ade05d5df1cf7025403e5b97f351794807c80508
Suman Chapai <sumanchapai@gmail.com>
2022-12-08 14:49:15 -0600

    Fix typo

--- usage/processing/check_flats_bias_count.py
+++ usage/processing/check_flats_bias_count.py
@@ -5,7 +5,7 @@
 from pathlib import Path
 
 # Change this
-yearFolderLocations = r"F:\Summer 2019"
+yearFolderLocation = r"F:\Summer 2019"
 
 # The code below is supposed to be static
 def handleDay(dayPath: Path) -> tuple[int]:
@@ -16,7 +16,7 @@ def handleDay(dayPath: Path) -> tuple[int]:
 
 
 def main():
-    root = Path(yearFolderLocations)
+    root = Path(yearFolderLocation)
     print(f"{'Day':20s}{'Flats':^6s}{'Bias':^6s}")
     f_total, b_total = 0, 0
     for day in root.iterdir():
