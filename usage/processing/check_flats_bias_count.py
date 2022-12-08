d4977558d654ea28b47417dfd205872b026f4140
Suman Chapai <sumanchapai@gmail.com>
2022-12-08 14:37:40 -0600

    Enhance script to calculate bias, flats length

--- usage/processing/check_flats_bias_count.py
+++ usage/processing/check_flats_bias_count.py
@@ -8,18 +8,22 @@ from pathlib import Path
 yearFolderLocations = r"F:\Summer 2019"
 
 # The code below is supposed to be static
-def handleDay(dayPath: Path):
+def handleDay(dayPath: Path) -> tuple[int]:
     flats = list(dayPath.glob("Calibration Frames/flat*.fit"))
     biases = list(dayPath.glob("Calibration Frames/bias*.fit"))
-    print(f"{dayPath.name:20s}{len(flats):<6}{len(biases):<6}")
+    print(f"{dayPath.name:20s}{len(flats):^6}{len(biases):^6}")
+    return len(flats), len(biases)
 
 
 def main():
     root = Path(yearFolderLocations)
-    print(f"{'Day':20s}{'Flats':6s}{'Bias':6s}")
+    print(f"{'Day':20s}{'Flats':^6s}{'Bias':^6s}")
+    f_total, b_total = 0, 0
     for day in root.iterdir():
-        handleDay(day)
-    pass
+        f, b = handleDay(day)
+        f_total += f
+        b_total += b
+    print(f"{'Total':20s}{f_total:^6}{b_total:^6}")
 
 
 if __name__ == "__main__":
