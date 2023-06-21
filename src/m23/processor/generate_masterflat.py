17bb5bd6d6d1b11c0f824106f0b0e75ff1af3353
suman chapai <sumanchapai@gmail.com>
2023-06-20 21:33:20 -0500

    Format

--- src/m23/processor/generate_masterflat.py
+++ src/m23/processor/generate_masterflat.py
@@ -5,16 +5,22 @@ from m23.constants import INPUT_CALIBRATION_FOLDER_NAME
 from m23.file.masterflat_file import MasterflatFile
 from m23.matrix import crop
 from m23.processor.generate_masterflat_config_loader import (
-    MasterflatGeneratorConfig, validate_generate_masterflat_config_file)
-from m23.utils import (fit_data_from_fit_images, get_darks,
-                       get_date_from_input_night_folder_name, get_flats)
+    MasterflatGeneratorConfig,
+    validate_generate_masterflat_config_file,
+)
+from m23.utils import (
+    fit_data_from_fit_images,
+    get_darks,
+    get_date_from_input_night_folder_name,
+    get_flats,
+)
 
 
 def generate_masterflat_auxiliary(config: MasterflatGeneratorConfig) -> None:
     """
     Generates masterflat based on the configuration provided
-    this function assumes that the configuration provided is valid as it 
-    should be only calledfrom generate_master_flat that checks for the validity 
+    this function assumes that the configuration provided is valid as it
+    should be only calledfrom generate_master_flat that checks for the validity
     of the configuration file before calling this function.
     """
     rows, cols = config["image"]["rows"], config["image"]["columns"]
@@ -27,7 +33,6 @@ def generate_masterflat_auxiliary(config: MasterflatGeneratorConfig) -> None:
     # Crop images if crop region is defined
     if len(crop_region) > 0:
         flats = [crop(matrix, rows, cols) for matrix in flats]
-
 
     # We have to first create master dark before creating masterflat
     # as masteflat requires masterdark. Note that we're passing saveAs
@@ -48,7 +53,7 @@ def generate_masterflat_auxiliary(config: MasterflatGeneratorConfig) -> None:
             get_flats(NIGHT_INPUT_CALIBRATION_FOLDER)
         ).absolute(),  # Gets absolute path of first flat file,
         listOfFlatData=flats,
-)
+    )
 
 
 def generate_masterflat(file_path: str):
