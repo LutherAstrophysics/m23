257239f1f612385ca06de8440b63b0604e81312f
Minh Nguyen <nguymi03@luther.edu>
2023-06-21 13:05:02 -0500

    tests: add test for fillMatrix

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -31,30 +31,31 @@ def test_block_regions():
     assert (boxes_multidim[1][0] == np.array([[16, 17], [24, 25]])).all()
 
 
-# def test_fill_matrix():
-#     rows, cols = 4, 8
-#     matrix = np.arange(rows * cols).reshape(rows, cols)
-#     # We test splitting a matrix vertically into horizontally
-#     # ----------------------------
-#     # | 0   1   2   3   4  5  6  7 |
-#     # | 8   9  10  11  12 13 14 15 |
-#     # | 16 17  18  19  20 21 22 23 |
-#     # | 24 25  26  27  28 29 30 31 |
-#     # -----------------------------
-#     # Let's test cropping a section of the matrix to turn it into
-#     # ----------------------------
-#     # | 0   1   2   3   4  5  6  7 |
-#     # | 8          11  12 13 14 15 |
-#     # | 16         19  20 21 22 23 |
-#     # | 24 25  26  27  28 29 30 31 |
-#     # -----------------------------
-#     polygon = [
-#         (1, 1),
-#         (1, 2),
-#         (2, 2),
-#         (2, 1),
-#     ]
-#     # Fill with 0
-#     polygons = [polygon]
-#     fillMatrix(np.arange(100 * 100).reshape(100, 100), [[]], 0)
-#     # Check that the original matrix is mutated
+def test_fill_matrix():
+    rows, cols = 10, 10
+    matrix = np.arange(rows * cols).reshape(rows, cols)
+    # We test splitting a matrix vertically into horizontally
+    # ----------------------------
+    # | 0   1   2   3   4  5  6  7 |
+    # | 8   9  10  11  12 13 14 15 |
+    # | 16 17  18  19  20 21 22 23 |
+    # | 24 25  26  27  28 29 30 31 |
+    # -----------------------------
+    # Let's test cropping a section of the matrix to turn it into
+    # ----------------------------
+    # | 0   1   2   3   4  5  6  7 |
+    # | 8          11  12 13 14 15 |
+    # | 16         19  20 21 22 23 |
+    # | 24 25  26  27  28 29 30 31 |
+    # -----------------------------
+    polygon = [
+        (0, 1),
+        (5, 1),
+        (3, 3),
+        (0, 1),
+    ]
+    # Fill with 0
+    polygons = [polygon]
+    fillMatrix(matrix, polygons, 1000)
+    assert matrix[1,1] == 1000
+
